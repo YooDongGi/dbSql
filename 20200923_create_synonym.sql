3e9aec9b46dd41778370247c46f436237f973339
dgYoo <db4906@gmail.com>
2020-09-23 18:16:07 +0900

    DDL  . index - table access  . dml 실행시 index 영향  . SYNONYM
    DCL
     . GRANT, REVOKE
     . 시스템권한, 객체권한
    
    DICATIONARY
    
    multiple insert
     . unconditional insert
     . conditional insert
    
    실습
     ind1~3

---
+++ 20200923_create_synonym.sql
@@ -0,0 +1,3 @@
+YDG, HR 계정에 SYNONYM을 생성할 수 있는 권한을 부여
+GRANT CREATE SYNONYM TO ydg;
+GRANT CREATE SYNONYM TO hr;
---
+++ 20200923_hr_create_synonym.sql
@@ -0,0 +1,5 @@
+SELECT * FROM ydg.v_emp;
+
+CREATE SYNONYM v_emp FOR ydg.v_emp;
+
+SELECT * FROM v_emp;
